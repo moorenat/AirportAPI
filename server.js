9ff7116a836ea88ccd2cd125bc7dd8a7caa3e0c2
Nate Moore <natemoore2088@gmail.com>
2021-12-04 16:53:27 -0700

    added most planes functionality

--- server.js
+++ server.js
@@ -14,11 +14,15 @@ const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 
 const PILOT = "Pilot";
+const PLANE = "Plane";
+const HANGER = "Hanger";
+
 
 const router = express.Router();
 const login = express.Router();
-const owner = express.Router();
-const callback = express.Router();
+const user = express.Router();
+const plane = express.Router();
+const hanger = express.Router();
 
 const CLIENT_ID = 'LNjMPnuvPFVQPsi2UZvCI4lhSZJobVDh';
 const CLIENT_SECRET = 'Smhe7JUl2seg75Xc5H6RMvc9q4u8QfKzjxNwGP3HYpFVaV21EILYlfJ4XGGTQ-Xb';
@@ -60,16 +64,19 @@ const checkJwt = jwt({
 });
 
 /* ------------- Begin Lodging Model Functions ------------- */
-function post_pilot(name, type, length, public, owner) {
+
+//datastore functions for pilots:
+
+function post_pilot(name, sub) {
     var key = datastore.key(PILOT);
-    const new_pilot = { "name": name, "type": type, "length": length, "public": public, "owner": owner };
+    const new_pilot = { "name": name, "sub": sub };
     return datastore.save({ "key": key, "data": new_pilot }).then(() => { return key });
 }
 
-function get_pilots(owner) {
+function get_pilots(sub) {
     const q = datastore.createQuery(PILOT);
     return datastore.runQuery(q).then((entities) => {
-        return entities[0].map(fromDatastore).filter(item => item.owner === owner);
+        return entities[0].map(fromDatastore).filter(item => item.sub === sub);
     });
 }
 
@@ -97,6 +104,50 @@ function delete_pilot(id) {
     return datastore.delete(key);
 }
 
+// Datastore Functions for planes:
+
+function post_plane(pilot, hanger, manufacturer, tailNo, color) {
+    var key = datastore.key(PLANE);
+    const new_plane = { "pilot": pilot, "hanger": hanger, "manufacturer": manufacturer, "tailNo": tailNo, "color": color };
+    return datastore.save({ "key": key, "data": new_plane }).then(() => { return key });
+}
+function get_plane(id) {
+    const key = datastore.key([PLANE, parseInt(id, 10)]);
+    return datastore.get(key).then((entity) => {
+        if (entity[0] === undefined || entity[0] === null) {
+            // No entity found. Don't try to add the id attribute
+            return entity;
+        } else {
+            // Use Array.map to call the function fromDatastore. This function
+            // adds id attribute to every element in the array entity
+            return entity.map(fromDatastore);
+        }
+    });
+}
+function get_planes_unprotected() {
+    const q = datastore.createQuery(PLANE);
+    return datastore.runQuery(q).then((entities) => {
+        return entities[0].map(fromDatastore);
+    });
+}
+function get_planes(pilot) {
+    const q = datastore.createQuery(PLANE);
+    return datastore.runQuery(q).then((entities) => {
+        return entities[0].map(fromDatastore).filter(item => item.pilot === pilot);
+    });
+}
+
+function patch_plane(id, pilot, hanger, manufacturer, tailNo, color, self) {
+    const key = datastore.key([PLANE, parseInt(id, 10)]);
+    const plane = { "pilot": pilot, "hanger": hanger, "manufacturer": manufacturer, "tailNo": tailNo, "color": color, "self": self };
+    return datastore.save({ "key": key, "data": plane });
+}
+
+function delete_planes(id) {
+    const key = datastore.key([PILOT, parseInt(id, 10)]);
+    return datastore.delete(key);
+}
+
 
 /* ------------- End Model Functions ------------- */
 
@@ -104,26 +155,19 @@ function delete_pilot(id) {
 
 router.delete('/:pilot_id', checkJwt, function (req, res) {
     const pilot_id = req.params.pilot_id
-    const owner = req.user.sub
+    const sub = req.user.sub
 
     const pilot = get_pilot(pilot_id).then((pilot) => {
         if (pilot[0] === undefined || pilot[0] === null) {
-            res.status(403).send('one').end()
+            res.status(403).json({ 'Error': 'Forbidden' })
         }
-        else if (pilot[0].owner === owner) {
+        else if (pilot[0].sub === sub) {
             delete_pilot(pilot_id).then(res.status(204).end())
         }
         else {
-            res.status(403).send('2').end()
+            res.status(403).json({ 'Error': 'Forbidden' })
         }
     })
-
-})
-
-callback.get('/', function (req, res) {
-
-
-
 })
 
 router.get('/', checkJwt, function (req, res) {
@@ -136,42 +180,133 @@ router.get('/', checkJwt, function (req, res) {
         });
 });
 
+router.post('/', checkJwt, function (req, res) {
+    if (req.get('content-type') !== 'application/json') {
+        res.status(415).json({ 'Error': 'Server only accepts application/json data.' })
+    }
+    post_pilot(req.body.name, req.user.sub)
+        .then(key => {
+            res.location(req.protocol + "://" + req.get('host') + req.baseUrl + '/' + key.id);
+            res.status(201).send('{ "id": ' + key.id + ' }')
+        });
+});
+router.put('/', function (req, res) {
+    res.set('Accept', 'GET, POST, DELETE');
+    res.status(405).json({ 'Error': '/pilots only alows GET, POST, DELETE' });
+});
+router.patch('/', function (req, res) {
+    res.set('Accept', 'GET, POST, DELETE');
+    res.status(405).json({ 'Error': '/pilots only alows GET, POST, DELETE' });
+});
 
+plane.get('/', checkJwt, async function (req, res) {
+    const pilot = req.user.sub
+    let planes = await get_planes(pilot)
+    if (planes[0] === undefined || planes[0] === null) {
+        res.status(404).json({ 'Error': 'No planes exist for this pilot' })
+    }
+    else {
+        res.status(200).json(planes)
+    }
+})
 
-router.get('/unsecure', function (req, res) {
+plane.get('/unsecure', async function (req, res) {
+    const accepts = req.accepts('application/json')
+    if (!accepts) {
+        res.status(406).json({ 'Error': 'accept header must be application/json' })
+    }
+    else {
+        const planes = await get_planes_unprotected()
+        res.status(200).json(planes)
+    }
+})
+plane.delete('/', function (req, res) {
+    res.set('Accept', 'GET, POST, PATCH');
+    res.status(405).json({ "Error": "Route only accetps GET, POST, PATCH. To Delete use /planes/:plane_id" })
+
+})
+
+plane.delete('/:plane_id', checkJwt, async function (req, res) {
+    const pilot = req.user.sub
+
+})
+
+plane.patch('/:plane_id', checkJwt, async function (req, res) {
+    const plane_id = req.params.plane_id
+    const pilot = req.user.sub
+
+    let plane = await get_plane(plane_id)
+
+    if (plane[0] === undefined || plane[0] === null) {
+        res.status(404).json({ 'Error': 'Plane not found' })
+    }
+    else if (plane[0].pilot === pilot) {
+        let { hanger, manufacturer, tailNo, color } = req.body
+        if (req.get('content-type') !== 'application/json') {
+            res.status(415).send('Server only accepts application/json data.')
+        }
+
+        else if (!hanger && !manufacturer && !tailNo && !color) {
+            return res.status(400).json({
+                Error: "The request object is missing at least one of the required attributes"
+            })
+        }
+        else if (Object.keys(req.body).length > 4) {
+            return res.status(400).json({
+                Error: 'There are too many attributes in request'
+            })
+        }
+        else {
+            plane = plane[0]
+            updatedPlane = Object.assign(plane, req.body)
+            planePatch = await patch_plane(plane_id, pilot, plane.hanger, plane.manufacturer, plane.tailNo, plane.color, plane.self)
+            res.status(200).json(plane)
+        }
+    }
+    else {
+        res.status(403).json({ 'Error': 'Forbidden' })
+    }
+});
+
+plane.get('/:plane_id', checkJwt, async function (req, res) {
+    const plane_id = req.params.plane_id
+    const pilot = req.user.sub
+
+    const plane = await get_plane(plane_id)
+    if (plane[0] === undefined || plane[0] === null) {
+        res.status(404).json({ 'Error': 'Plane not found' })
+    }
+    else if (plane[0].pilot === pilot) {
+        res.status(200).json(plane[0])
+    }
+    else {
+        res.status(403).json({ 'Error': 'Forbidden' })
+    }
+})
+
+plane.post('/', checkJwt, async function (req, res) {
+
+    if (req.get('content-type') !== 'application/json') {
+        res.status(415).json({ 'Error': 'Server only accepts application/json data' })
+    }
+    else {
+        const plane = await post_plane(req.user.sub, req.body.hanger, req.body.manufacturer, req.body.tailNo, req.body.color)
+        let url = req.protocol + "://" + req.get("host") + "/planes/" + plane.id
+        let output = { "id": plane.id, "pilot": req.user.sub, "hanger": req.body.hanger, "manufacturer": req.body.manufacturer, "tailNo": req.body.tailNo, "color": req.body.color, "self": url }
+        const planeUpdate = await patch_plane(plane.id, req.user.sub, req.body.hanger, req.body.manufacturer, req.body.tailNo, req.body.color, url)
+        res.status(201).json(output)
+    }
+
+})
+
+user.get('/', function (req, res) {
     const pilots = get_pilots_unprotected()
         .then((pilots) => {
             res.status(200).json(pilots);
         });
 });
 
-owner.get('/:owner_id/pilots', function (req, res) {
-    const owner = req.params.owner_id
-    const pilotArr = []
-    console.log(owner)
-    const pilots = get_pilots_unprotected(owner)
-        .then((pilots) => {
-            for (pilot = 0; pilot < pilots.length; pilot++) {
-                if (pilots[pilot].owner === owner && pilots[pilot].public) {
-                    pilotArr.push(pilots[pilot])
-                    console.log(pilotArr)
-                }
-            }
-            res.status(200).json(pilotArr)
-        });
-})
-    ;
 
-router.post('/', checkJwt, function (req, res) {
-    if (req.get('content-type') !== 'application/json') {
-        res.status(415).send('Server only accepts application/json data.')
-    }
-    post_pilot(req.body.name, req.body.type, req.body.length, req.body.public, req.user.sub)
-        .then(key => {
-            res.location(req.protocol + "://" + req.get('host') + req.baseUrl + '/' + key.id);
-            res.status(201).send('{ "id": ' + key.id + ' }')
-        });
-});
 
 login.post('/', function (req, res) {
     const username = req.body.username;
@@ -201,10 +336,11 @@ login.post('/', function (req, res) {
 });
 
 /* ------------- End Controller Functions ------------- */
-app.use('/owners', owner);
+app.use('/users', user);
 app.use('/pilots', router);
+app.use('/planes', plane)
 app.use('/login', login);
-app.use('/callback', callback);
+app.use('/hangers', hanger);
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
